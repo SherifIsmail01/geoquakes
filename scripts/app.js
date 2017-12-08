fd96061c65e52b6cf32b46d01175d1552bfaa4c5
Sherif Ismail <sherifismail@proslamx.local>
2017-12-08 11:00:32 -0800

    map created

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,27 @@
 // define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
-
+var source ="https://maps.googleapis.com/maps/api/js?key=AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg"
 $(document).ready(function() {
   console.log("Let's get coding!");
   // CODE IN HERE!
+  function initMap() {
+
+  var sanFrancisco = {lat: 37.78, lng: -122.44};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: sanFrancisco
+  });
+}
+initMap();
+
+
+
+
+
+
+
+
+
 
 });
